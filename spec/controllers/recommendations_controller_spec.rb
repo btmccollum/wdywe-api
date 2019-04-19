f7864a4639dd70d614c7d128010663132598ed57
btmccollum <btmcco8@gmail.com>
2019-04-19 16:36:26 -0500

    updating controller test spec

--- spec/controllers/recommendations_controller_spec.rb
+++ spec/controllers/recommendations_controller_spec.rb
@@ -17,36 +17,16 @@ RSpec.describe Api::V1::RecommendationsController, type: :controller do
             it 'returns a usable hash from param data' do
                 test_response = controller.send(:search_query, @parameters)
 
-                expect (test_response.keys).to include("term", "location", "radius", "limit", "open_now")
-                expect (test_response).to match({
-                    "term"=> "bbq",
-                    "location"=> "fort worth",
-                    "radius"=> "30000",
-                    "limit"=> "100",
-                    "open_now"=>"true"
+                expect(test_response.keys).to include(:term, :location, :radius, :limit, :open_now)
+                expect(test_response).to match({
+                    :term => "bbq",
+                    :location => "fort worth",
+                    :radius => "30000",
+                    :limit => "100",
+                    :open_now =>"true"
                 })
             end
         end
     end
-
-    # describe 'POST #random_suggestion' do
-    #     context 'when asking for a guided suggestion' do
-    #         it 'creates a search_data object from params' do
-    #             post 'random_suggestion', :params => @parameters
-
-    #             hash_body = nil
-            
-    #             expect { hash_body = JSON.parse(response.body).with_indifferent_access }.not_to raise_exception
-    #             expect(hash_body.keys).to include("term", "location", "radius", "limit", "open_now")
-    #             expect(hash_body).to match({
-    #                 "term"=> "bbq",
-    #                 "location"=> "fort worth",
-    #                 "radius"=> "30000",
-    #                 "limit"=> "100",
-    #                 "open_now"=>"true"
-    #             })
-    #         end
-    #     end
-    # end
 
 end
