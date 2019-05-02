0822977d498cad5790e1956ef36d42c58c3edc64
btmccollum <btmcco8@gmail.com>
2019-05-01 19:53:12 -0500

    Adding start of tests for app controller

--- spec/controllers/application_controller_spec.rb
+++ spec/controllers/application_controller_spec.rb
@@ -1,20 +1,25 @@
-# require 'rails_helper'
+require 'rails_helper'
 
-# RSpec.describe ApplicationController, type: :controller do 
+RSpec.describe ApplicationController, type: :controller do 
 
-#     let(:user) { FactoryGirl.create(:user) }
-     
-#     before do 
-#         @current_user = 
+    before(:each) do
+        @params = {
+            "username"=>"test01",
+            "email"=>"test01@test.com",
+            "password"=>"123456", 
+            "password_confirmation"=> "123456",
+            "zipcode"=>"12345"
+        }   
+        @user = User.create(@params)
+    end
 
-#     end
+    describe 'authenticate' do 
+        context 'when application is being accessed' do
+            it "returns 401 when unauthorized user tries to access" do
+                test_response = controller.send(:current_user)
 
-#     describe 'authenticate' do 
-#         context 'when application is being accessed' do
-#             it 'returns a 401 error if accessed by an unauthorized user' do
-
-
-#             end
-#         end
-#     end
-# end
+                expect(test_response).to be(true)
+            end
+        end
+    end
+end
